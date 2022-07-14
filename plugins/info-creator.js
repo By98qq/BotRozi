f67a3e8e320a5e9e1acec292f27a646792cfc27e
Breder12345Aa <82520506+Breder12345Aa@users.noreply.github.com>
2022-07-14 15:42:32 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -3,13 +3,13 @@ let handler = async (m, {conn}) => {
 const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:;;;
-FN: ᴹᴿ᭄ ZifaOffcོ_1ঔৣ꧂
-item.ORG: ᴹᴿ᭄ ZifaOffcོ_1ঔৣ꧂
-item1.TEL;waid=6285828377610:6285828377610@s.whatsapp.net
+FN: ᴹᴿ᭄B O T-Offcོ_1ঔৣ꧂
+item.ORG: ᴹᴿ᭄BOT-vcོ_1ঔৣ꧂
+item1.TEL;waid=6283183321692:6283183321692@s.whatsapp.net
 item1.X-ABLabel:owner selalu ada 
-item2.EMAIL;type=INTERNET: anidaanida59@gmail.com
+item2.EMAIL;type=INTERNET: xiomi78aa@gmail.com
 item2.X-ABLabel:📧Email
-item3.ADR:;;🎍Pontianak;;;;
+item3.ADR:;;🎍Medan;;;;
 item3.X-ABADR:ac
 item3.X-ABLabel:asal kota
 item4.URL:https//github.com/Botynyakamu
